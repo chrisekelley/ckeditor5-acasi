eb6a7b4b6f14b4c9def899dbbaf5d31df3ecf4fb
Chris E. Kelley <chris@vetula.com>
2017-11-08 15:01:19 +0100

    re-name web component to tangy-acasi

--- src/acasi.js
+++ src/acasi.js
@@ -41,22 +41,22 @@ export default class Acasi extends Plugin {
     schema.allow( { name: 'image', inside: 'paper-radio-button' } );
     schema.objects.add( 'paper-radio-button' );
 
-    schema.registerItem( 'acasi' );
-    schema.allow( { name: 'acasi', attributes: [ 'intro-src' ], inside: '$root' } );
-    // schema.allow( { name: 'acasi', inside: '$root' } );
-    schema.allow( { name: '$inline', inside: 'acasi' } );
-    schema.allow( { name: 'image', inside: 'acasi' } );
-    schema.allow( { name: 'paper-radio-button', inside: 'acasi' } );
-    schema.objects.add( 'acasi' );
+    schema.registerItem( 'tangy-acasi' );
+    schema.allow( { name: 'tangy-acasi', attributes: [ 'intro-src' ], inside: '$root' } );
+    // schema.allow( { name: 'tangy-acasi', inside: '$root' } );
+    schema.allow( { name: '$inline', inside: 'tangy-acasi' } );
+    schema.allow( { name: 'image', inside: 'tangy-acasi' } );
+    schema.allow( { name: 'paper-radio-button', inside: 'tangy-acasi' } );
+    schema.objects.add( 'tangy-acasi' );
 
     // Build converter from model element to view element is used to render the getData output for the widget when you create new Elements in the editor.
     buildModelConverter().for( data.modelToView )
-      .fromElement( 'acasi' )
-      // .toElement( 'acasi');
+      .fromElement( 'tangy-acasi' )
+      // .toElement( 'tangy-acasi');
       .toElement( (element) => {
         const introSrc = element.item.getAttribute('intro-src')
-        // return new ViewContainerElement( 'acasi', { class: 'acasi' }, [new ViewEmptyElement( 'img' ), new ViewEmptyElement( 'img' ), new ViewEmptyElement( 'img' ), new ViewEmptyElement( 'img' )] );
-        let container = new ViewContainerElement( 'acasi', {'intro-src': introSrc} );
+        // return new ViewContainerElement( 'tangy-acasi', { class: 'tangy-acasi' }, [new ViewEmptyElement( 'img' ), new ViewEmptyElement( 'img' ), new ViewEmptyElement( 'img' ), new ViewEmptyElement( 'img' )] );
+        let container = new ViewContainerElement( 'tangy-acasi', {'intro-src': introSrc} );
         return container
       })
     buildModelConverter().for( data.modelToView )
@@ -64,22 +64,22 @@ export default class Acasi extends Plugin {
       .toElement( (element) => {
         const name = element.item.getAttribute('name')
         const value = element.item.getAttribute('value')
-        // return new ViewContainerElement( 'acasi', { class: 'acasi' }, [new ViewEmptyElement( 'img' ), new ViewEmptyElement( 'img' ), new ViewEmptyElement( 'img' ), new ViewEmptyElement( 'img' )] );
+        // return new ViewContainerElement( 'tangy-acasi', { class: 'tangy-acasi' }, [new ViewEmptyElement( 'img' ), new ViewEmptyElement( 'img' ), new ViewEmptyElement( 'img' ), new ViewEmptyElement( 'img' )] );
         let container = new ViewContainerElement( 'paper-radio-button', {'name': name, 'value': value} );
         return container
       })
 
     //  Build converter from model element to view element for editing view pipeline. This affects how this element is rendered in the editor.
     buildModelConverter().for( editing.modelToView )
-      .fromElement( 'acasi' )
+      .fromElement( 'tangy-acasi' )
       .toElement( (element) => {
         // const imageContainer = createImageViewElement();
-        console.log("acasi element")
-        const figureContainer1 = new ViewContainerElement( 'figure', { class: 'acasi' });
-        const figureContainer2 = new ViewContainerElement( 'figure', { class: 'acasi' });
-        const figureContainer3 = new ViewContainerElement( 'figure', { class: 'acasi' });
-        const figureContainer4 = new ViewContainerElement( 'figure', { class: 'acasi' });
-        const widgetContainer = new ViewContainerElement( 'figure', { contenteditable: 'true' },
+        console.log("tangy-acasi element")
+        const figureContainer1 = new ViewContainerElement( 'figure', { class: 'paper-radio-button' });
+        const figureContainer2 = new ViewContainerElement( 'figure', { class: 'paper-radio-button' });
+        const figureContainer3 = new ViewContainerElement( 'figure', { class: 'paper-radio-button' });
+        const figureContainer4 = new ViewContainerElement( 'figure', { class: 'paper-radio-button' });
+        const widgetContainer = new ViewContainerElement( 'figure', { class: 'tangy-acasi' },
           [figureContainer1, figureContainer2, figureContainer3, figureContainer4] );
         const widget = toWidget( widgetContainer );
         widget.setAttribute( 'contenteditable', true );
@@ -89,7 +89,7 @@ export default class Acasi extends Plugin {
 
     //  Build converter from model element to view element for editing view pipeline. This affects how this element is rendered in the editor.
     // It is not necessary to render this element.
-    // todo: nest this inside acasi.
+    // todo: nest this inside tangy-acasi.
     buildModelConverter().for( editing.modelToView )
       .fromElement( 'paper-radio-button' )
       .toElement( (element) => {
@@ -103,18 +103,18 @@ export default class Acasi extends Plugin {
     // Build converter from view element to model element for data pipeline. When the editor is consuming html,
     // recognize your widget and put it in the model so that editing.modelToView can then render it in the editing view
     buildViewConverter().for( data.viewToModel )
-      .fromElement( 'acasi' )
+      .fromElement( 'tangy-acasi' )
       .toElement( () => {
         const imageUrl = 'assets/babycat.jpg';
         const imageElement = new ModelElement( 'image', {
           src: imageUrl
         });
-        new ModelElement( 'acasi' ), {'intro-src':'assets/sounds/1.mp3'}, [imageElement]
+        new ModelElement( 'tangy-acasi' ), {'intro-src':'assets/sounds/1.mp3'}, [imageElement]
       });
 
     // // Build converter for view img element to model image element.
     // buildViewConverter().for( data.viewToModel )
-    //   .from( { name: 'acasi', attribute: { 'intro-src': /./ } } )
+    //   .from( { name: 'tangy-acasi', attribute: { 'intro-src': /./ } } )
     //   .toElement( (viewImage) =>  {
     //     console.log("getting intro-src")
     //     new ModelElement( 'image', { 'intro-src': viewImage.getAttribute( 'intro-src' ) } )
@@ -123,7 +123,7 @@ export default class Acasi extends Plugin {
     // createImageAttributeConverter( [ editing.modelToView, data.modelToView ], 'src' );
 
 
-    // Add acasi button to feature components.
+    // Add tangy-acasi button to feature components.
     editor.ui.componentFactory.add( 'acasi', locale => {
       const view = new ButtonView(locale);
       view.set({
@@ -158,8 +158,8 @@ export default class Acasi extends Plugin {
 
           // // const widgetElement = new ModelElement('figure', { class: 'fancy-widget' },imageElement)
           // editor.data.insertContent( imageElement, editor.document.selection );
-          const acasi = new ModelElement( 'acasi', {'intro-src':url}, [prb1, prb2, prb3, prb4])
-          // const acasi = new ModelElement( 'acasi', { src: imageUrl });
+          const acasi = new ModelElement( 'tangy-acasi', {'intro-src':url}, [prb1, prb2, prb3, prb4])
+          // const acasi = new ModelElement( 'tangy-acasi', { src: imageUrl });
           editor.data.insertContent( acasi, editor.document.selection );
         } );
       });
